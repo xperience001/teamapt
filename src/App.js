96961efee8365cffe4469bae3852f15e84cfe7d9
Habeeb <tobilobatijani2011@gmail.com>
2022-02-07 06:04:07 +0100

    stillinprogress

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
-import './App.scss';
-import first from './images/eeeee.jpg';
-import second from './images/eeree.jpg';
-import third from './images/f.jpg';
-import fourth from './images/ff.jpg';
-import fifth from './images/frrr.jpg';
+import { useState } from "react";
+import "./App.scss";
+import first from "./images/eeeee.jpg";
+import second from "./images/eeree.jpg";
+import third from "./images/f.jpg";
+import fourth from "./images/ff.jpg";
+import fifth from "./images/frrr.jpg";
 
 import { Link } from "react-router-dom";
 
 function App() {
+  const items = [
+    { name: "CARE", image: first },
+    { name: "HOTELS", image: second },
+    { name: "LOUNGE", image: third },
+    { name: "SUITES", image: first },
+    { name: "GUESTS", image: fourth },
+    { name: "LAND", image: fifth },
+  ];
+  const [active, setActive] = useState();
+
   return (
     <div className="Apps">
       <header className="App-header">
-        <Link to="first" style={{ textDecoration: 'none' }} to="/"><nav className='logo'><img style={{ height: '3rem', width: '3rem', marginLeft: '2rem', marginTop: '2rem' }} src={second} /></nav></Link>
-        <div className='socio'>
+        <Link to="first" style={{ textDecoration: "none" }} to="/">
+          <nav className="logo">
+            <img src={second} />
+          </nav>
+        </Link>
+        <div className="socio">
           <nav>Facebook</nav>
           <nav>LinkedIn</nav>
           <nav>YouTube</nav>
         </div>
-        <span className='get'>
-          Get the full report
-        </span>
+        <span className="get">Get the full report</span>
       </header>
-      <h1 className='sectors'>OUR SECTORS</h1>
-      <div className='frames' style={{ marginTop: '-2rem' }}>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'>
-          <span className='num'>
-            <h1 className='numnum'>01</h1>
-            <h2 className='numword'>AX CARE</h2>
-          </span>
-          <img src={first} /></div></Link>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'><span className='num'>           <h1 className='numnum'>02</h1>
-          <h2 className='numword'>AX HOTELS</h2></span><img src={second} /></div></Link>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'><span className='num'>           <h1 className='numnum'>03</h1>
-          <h2 className='numword'>AX LOUNGE</h2></span><img src={third} /></div></Link>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'><span className='num'>           <h1 className='numnum'>04</h1>
-          <h2 className='numword'>AX SUITES</h2></span><img src={first} /></div></Link>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'><span className='num'>           <h1 className='numnum'>05</h1>
-          <h2 className='numword'>AX GUESTS</h2></span><img src={fourth} /></div></Link>
-        <Link to="first" style={{ textDecoration: 'none' }} to="/view"><div className='imgdiv'><span className='num'>           <h1 className='numnum'>06</h1>
-          <h2 className='numword'>AX LAND</h2></span><img src={fifth} /></div></Link >
+      <h1 className="sectors">OUR SECTORS</h1>
+      <div className="frames" style={{ marginTop: "-2rem" }}>
+        {items.map((item, index) => (
+          <Link
+            to="first"
+            style={{ textDecoration: "none" }}
+            to="/view"
+            key={index}
+          >
+            <div
+              className={`imgdiv imgdiv-${index}${
+                index === active ? " active" : ""
+              }`}
+              onMouseEnter={(_) => setActive(index)}
+              onMouseLeave={(_) => setActive()}
+            >
+              <span className="num">
+                <h2 className="numnum">0{index + 1}</h2>
+                <h2 className="numword">
+                  AX
+                  <br />
+                  {item.name}
+                </h2>
+              </span>
+              <div
+                className="img"
+                style={{
+                  backgroundImage: `url("${item.image}")`,
+                }}
+              />
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
